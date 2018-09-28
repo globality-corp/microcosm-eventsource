7971035daad9a5c6aa1e977cbdab7127e6c535b6
pelegdvir <gaspipe@gmail.com>
2018-09-28 09:44:39 -0700

    Support deferred-batch (#44)

--- microcosm_eventsource/routes.py
+++ microcosm_eventsource/routes.py
@@ -6,16 +6,25 @@ from microcosm_flask.conventions.base import EndpointDefinition
 from microcosm_flask.conventions.crud import configure_crud
 from microcosm_flask.operations import Operation
 from microcosm_postgres.context import transactional
+from microcosm_pubsub.producer import deferred_batch
 
 
-def configure_event_crud(graph,
-                         controller,
-                         event_schema,
-                         new_event_schema,
-                         search_event_schema):
+def configure_event_crud(
+    graph,
+    controller,
+    event_schema,
+    new_event_schema,
+    search_event_schema,
+    use_deferred_batch=False,
+):
+    if use_deferred_batch:
+        create_func = transactional(deferred_batch(controller)(controller.create))
+    else:
+        create_func = transactional(controller.create)
+
     mappings = {
         Operation.Create: EndpointDefinition(
-            func=transactional(controller.create),
+            func=create_func,
             request_schema=new_event_schema,
             response_schema=event_schema,
         ),
